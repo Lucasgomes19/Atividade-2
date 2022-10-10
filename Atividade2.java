d508c54b7efa966c45b089497854d9643e4e37e3
Luckz Madrid <Lucasgomes19>
2022-10-10 17:23:25 -0300

    Atividade 2

---
+++ Atividade2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Atividade2{
+
+    
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Informe o número: ");
+
+    int número = sc.nextInt();
+    System.out.print("O número informado foi " + número);
+
+    sc.close();
+    
+    }
+}
